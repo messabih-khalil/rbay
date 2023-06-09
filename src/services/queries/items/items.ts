93e696c1b67985cb5f7df1e6b50794b79011dbb1
messabih-khalil <msbih.khalil@gmail.com>
2023-06-09 05:44:20 +0100

    feat(items): add support for creating and retrieving items from Redis cache
    This commit adds support for creating and retrieving items from Redis cache. The `createItem` function now generates a unique id for the item and serializes the attributes before storing them in Redis. The `getItem` function retrieves the item from Redis and deserializes it before returning it. The `serialize` and `deserialize` functions are used to convert the attributes to and from a format that can be stored in Redis.

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,27 @@
 import type { CreateItemAttrs } from '$services/types';
+import { serialize, deserialize } from './serialize';
+import { client } from '$services/redis';
 
-export const getItem = async (id: string) => {};
+import { genId } from '$services/utils';
+import { itemsKey } from '$services/keys';
+export const getItem = async (id: string) => {
+	const items = await client.hGetAll(itemsKey(id));
+
+	if (Object.keys(items).length === 0) {
+		return null;
+	}
+
+	return deserialize(items);
+};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs) => {
+	const id = genId();
+
+	const serialized = serialize(attrs);
+
+	await client.hSet(itemsKey(id), serialized);
+
+	return id;
+};
--- src/services/queries/items/serialize.ts
+++ src/services/queries/items/serialize.ts
@@ -1,3 +1,22 @@
 import type { CreateItemAttrs } from '$services/types';
+import { DateTime } from 'luxon';
 
-export const serialize = (attrs: CreateItemAttrs) => {};
+export const serialize = (attrs: { [key: string]: any }) => {
+	return {
+		...attrs,
+		createdAt: attrs.createdAt.toMillis(),
+		ending: attrs.endingAt.toMillis()
+	};
+};
+
+export const deserialize = (attrs: { [key: string]: any }) => {
+	return {
+		...attrs,
+		createdAt: DateTime.fromMillis(parseInt(attrs.createdAt)),
+		views: parseInt(attrs.view),
+		likes: parseInt(attrs.likes),
+		bids: parseInt(attrs.bids),
+		price: parseFloat(attrs.price),
+		endingAt: DateTime.fromMillis(parseInt(attrs.endingAt))
+	};
+};
