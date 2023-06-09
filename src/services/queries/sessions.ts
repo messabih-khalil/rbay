ae6974651d32ab797fc214477f003d58f20b1fec
messabih-khalil <msbih.khalil@gmail.com>
2023-06-09 05:44:57 +0100

    refactor(sessions.ts): extract session deserialization logic into a separate function for better code organization and readability feat(sessions.ts): implement saveSession function to save session data to Redis using a generated id feat(sessions.ts): implement getSession function to retrieve session data from Redis using the provided id and deserialize it using the deserialize function

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,28 @@
 import type { Session } from '$services/types';
+import { client } from '$services/redis';
+import { sessionsKey } from '$services/keys';
+import { genId } from '../utils/gen-id';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+	const session = await client.hGetAll(sessionsKey(id));
+	if (Object.keys(session).length === 0) {
+		return null;
+	}
+	return deserialize(id, session);
+};
 
-export const saveSession = async (session: Session) => {};
+export const saveSession = async (session: Session) => {
+	const id = genId();
+	return await client.hSet(sessionsKey(id), {
+		username: session.username,
+		userId: session.userId
+	});
+};
+
+const deserialize = (id: string, session: { [key: string]: string }) => {
+	return {
+		id: id,
+		userId: session.id,
+		username: session.username
+	};
+};
