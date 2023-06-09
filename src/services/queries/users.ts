5502c9f053b27e938a83aa359bb4cea017801e2e
messabih-khalil <msbih.khalil@gmail.com>
2023-06-09 04:33:17 +0100

    feat(users.ts): add implementation for createUser function to store user data in Redis and return the generated user id

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,17 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { client } from '$services/redis';
+import { userKey } from '../keys';
 
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+	const id = genId();
+	await client.hSet(userKey(id), {
+		...attrs
+	});
+
+	return id;
+};
