9f4664e00ae68d40a4cc0c210ddaea6d724856e3
messabih-khalil <msbih.khalil@gmail.com>
2023-06-09 05:45:12 +0100

    refactor(users.ts): refactor getUserById function to use deserializer function to return a user object with id and attributes feat(users.ts): add serializer and deserializer functions to convert user object to and from Redis hash format

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -5,13 +5,29 @@ import { userKey } from '../keys';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(userKey(id));
+
+	return deserilizer(user, id);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
-	await client.hSet(userKey(id), {
-		...attrs
-	});
+	await client.hSet(userKey(id), serializer(attrs));
 
 	return id;
+};
+
+const serializer = (user: CreateUserAttrs) => {
+	return {
+		username: user.username,
+		password: user.password
+	};
+};
+
+const deserilizer = (user: { [key: string]: string }, userId: string) => {
+	return {
+		id: userId,
+		...user
+	};
 };
