c712f25029ad6fb4645e29e8171c13deaac73d04
messabih-khalil <msbih.khalil@gmail.com>
2023-06-09 05:44:39 +0100

    feat(keys.ts): add sessionsKey and itemsKey functions to generate keys for session and item data in Redis cache fix(keys.ts): change parameter name from userID to userId in userKey function to improve semantics

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,3 +1,5 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 
-export const userKey = (userID: string) => `users#${userID}`;
+export const userKey = (userId: string) => `users#${userId}`;
+export const sessionsKey = (sessionId: string) => `session#${sessionId}`;
+export const itemsKey = (itemId: string) => `items#${itemId}`;
